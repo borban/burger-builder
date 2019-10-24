04f4896316e32195ae1aa757f7ced364315aeebf
Ben Orban <benjamin.orban@ehi.com>
2019-10-24 13:43:51 -0500

    Lesson 256

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -88,25 +88,6 @@ class BurgerBuilder extends Component {
 
   purchaseContinueHandler = () => {
     this.setState({ loading: true });
-    // const order = {
-    //   ingredients: this.state.ingredients,
-    //   price: this.state.totalPrice,
-    //   customer: {
-    //     name: "Ben Orban",
-    //     address: {
-    //       street: "Teststreet 1",
-    //       zipCode: "62002",
-    //       country: "USA"
-    //     },
-    //     email: "test@test.com",
-    //     deliveryMethod: "fastest"
-    //   }
-    // };
-    //
-    // axios
-    //   .post("/orders.json", order)
-    //   .then(response => this.setState({ loading: false, purchasing: false }))
-    //   .catch(error => this.setState({ loading: false, purchasing: false }));
     const queryParams = [];
     for (let i in this.state.ingredients) {
       queryParams.push(
@@ -115,6 +96,7 @@ class BurgerBuilder extends Component {
           encodeURIComponent(this.state.ingredients[i])
       );
     }
+    queryParams.push("price=" + this.state.totalPrice);
     const queryString = queryParams.join("&");
 
     this.props.history.push({
