131ca3471ab85e18692c663b3be70c1104d3a290
Ben Orban <benjamin.orban@ehi.com>
2019-10-24 08:14:57 -0500

    Lesson 254

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -118,7 +118,7 @@ class BurgerBuilder extends Component {
     const queryString = queryParams.join("&");
 
     this.props.history.push({
-      pathName: "/checkout",
+      pathname: "/checkout",
       search: "?" + queryString
     });
   };
